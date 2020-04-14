f5bb204e74bc3f31351a18714e7e0f8e9c00cdd3
Matej Košiarčik <matej.kosiarcik@gmail.com>
2020-04-14 14:32:09 +0200

    refactor: general cleanup of sourcecode

---
+++ src/main.py
@@ -0,0 +1,124 @@
+#!/usr/bin/env python
+
+import argparse
+import sys
+import os
+import subprocess
+import shutil
+import functools
+from typing import List, Iterable
+import logging
+import itertools
+
+log = logging.getLogger('main')
+
+
+# Main function
+def main(argv: List[str]) -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument( '-n', '--dry-run', action='store_true',
+                        help='Don\'t remove files, only print what would be deleted')
+    parser.add_argument('-v', '--verbose', action='store_true',
+                        help='Additional logging output')
+    parser.add_argument('directories', nargs='*', help='Directories to clear')
+    # TODO: add -i/--interactive flag (similar to `git clean -i`)
+    # TODO: add -q/--quiet flag
+    # TODO: add -V/--version flag (probably after first deployment)
+    args = parser.parse_args(argv)
+
+    if args.verbose and args.quiet:
+        print('Can\'t accept both "quiet" and "verbose" flags.', file=sys.stderr)
+        return 1
+
+    # setup logging
+    log.setLevel(logging.WARN)
+    if args.verbose:
+        log.setLevel(logging.INFO)
+    log.addHandler(logging.StreamHandler())  # stderr
+
+    directories = args.directories
+    if directories is None:
+        log.info('No directory given. Using "."')
+        directories = ['.']
+    directories = [x for x in directories if len(x) > 0]
+    assert len(directories) > 0
+
+    dry_run = args.dry_run
+    for directory in directories:
+        process_directory(directory, dry_run)
+    return 0
+
+
+# Process single directory
+def process_directory(directory: str, dry_run: bool):
+    if not os.path.exists(directory):
+        log.info(f'Could not find "{directory}"')
+        return
+    log.info(f'Processing {directory}')
+
+    for file in find_files(directory):
+        if dry_run:
+            print(f'Would remove {file}')
+        else:
+            print(f'Removing {file}')
+            try:
+                delete(file)
+            except FileNotFoundError:
+                pass
+
+    log.info(f'Processed {directory}')
+    log.info('')
+
+
+# deletes file(or folder/symlink) from filesystem
+def delete(entry: str):
+    path = os.path.realpath(entry)
+    if os.path.isdir(path):
+        shutil.rmtree(entry)
+    elif os.path.isfile(path):
+        os.remove(entry)
+    else:
+        raise FileNotFoundError(f'file "${entry}" not exists')
+
+
+def find_files(dir: str) -> Iterable[str]:
+    command = ['find', dir] + files_for_find()
+    process = subprocess.Popen(command, stdout=subprocess.PIPE)
+    for line in process.stdout:
+        file = str(line.decode('utf-8').strip())
+        yield file
+    process.communicate()
+
+
+# returns generic list of files to add to "find" command
+def files_for_find() -> Iterable[str]:
+    files = [
+        '.DS_Store',  # macOS
+        '.localized',  # macOS
+        'CMakeCache.txt',  # cmake
+        '._*',  # dotbar macos/BSD files
+    ]
+    files = (['-name', x, '-type', 'f'] for x in files)
+
+    folders = [
+        # 'dist',  # general, node
+        # 'public',  # general, node
+        # 'build',  # general
+        'node_modules',  # npm, yarn
+        'bower_components',  # bower
+        '.build',  # swift package manager
+        'Pods',  # cocoapods (obj-c, swift)
+        'Carthage',  # carthage (obj-c, swift)
+        # 'target',  # rust
+        'CMakeFiles',  # cmake
+        'CMakeScripts',  # cmake
+        'venv',  # python (virtualenv, pyenv)
+        '.venv',  # python (virtualenv, pyenv)
+    ]
+    folders = (['-name', x, '-type', 'd', '-prune'] for x in folders)
+
+    return functools.reduce(lambda all, el: all + ['-or'] + el, itertools.chain(folders, files))
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
