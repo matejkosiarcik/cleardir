80332e84462c3d16352219b12c1ff628d6b81f7c
Matej Košiarčik <matej.kosiarcik@gmail.com>
2020-04-16 11:06:55 +0200

    feat: add __future__ imports for compatibility with python2

--- src/main.py
+++ src/main.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 # TODO: apply formatting and linting (combination of pep8, yapf, black, pylint, pycodestyle)
 
+from __future__ import (
+    absolute_import, division, print_function, unicode_literals
+)
 import argparse
 import sys
 import os
