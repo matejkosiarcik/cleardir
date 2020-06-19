cf2259b087846812789dc923e0a798e018ee12c9
Matej Košiarčik <matej.kosiarcik@gmail.com>
2020-06-19 13:41:03 +0200

    style: fix formatting

--- tests-cli/helpers.bash
+++ tests-cli/helpers.bash
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 # test the number of files left in DIR is N
-function count_files() {
+function count_files {
     [ "$(ls -A "${tmpdir}" | wc -l)" -eq "${1}" ]
 }
 
 # test output contains N number of "remove `file`" lines
-function count_output() {
+function count_output {
     [ "$(grep -iE 'remov((ed?)|(ing))' <<<"${output}" | wc -l)" -eq "${1}" ]
 }
